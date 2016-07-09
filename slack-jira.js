5e0287e45b7b866fce6e886c9f69b123c9afce3d
Darren Phillips <d2@keepersecurity.com>
2016-07-08 17:36:02 -0700

    1st

---
+++ slack-jira.js
@@ -0,0 +1,7 @@
+$reply = "Whatever you want";
+    $data = array(
+        "response_type"=>"in_channel",
+        "text"=>$repy,
+        );
+    header('Content-Type: application/json');
+    echo json_encode($data);
